94cbb668b314f698a8106994f24a1881b9c33f92
Meesum Abbas <79690513+meesum5445@users.noreply.github.com>
2023-05-31 01:23:18 +0500

    slider calculations

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+
+
+
 function convertToVid()
 {
     document.getElementById("right_cont1").style.display = "none";
     document.getElementById("cont1_video").style.display = "block";
 }
 
+// let arrayofanimatedpart=document.querySelectorAll(".animatedpart");
 
-let arrayofanimatedpart=document.getElementsByClassName('.animatedpart');
-arrayofanimatedpart.forEach((element,index)=> {
-    element.style.transform ='translateX(index*100%)'
-});
-
-
-
+// arrayofanimatedpart.forEach((element,index)=> {
+//     element.style.left= `${index *90+10}%`;
+// });
 
