673e77a7a469ec3d38f83bb114fcc1fe68414e75
Meesum Abbas <79690513+meesum5445@users.noreply.github.com>
2023-05-30 22:20:21 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function convertToVid()
+{
+    // alert("Hello! I am an alert box!!");
+    // e.preventDefault();
+    document.getElementById("right_cont1").style.display = "none";
+    document.getElementById("cont1_video").style.display = "block";
+}
+
+
+
+
+
